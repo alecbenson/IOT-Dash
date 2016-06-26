a3ab3f96b1d0afd7630e7431eed15319d06c2a55
alec <abenson@wpi.edu>
2016-06-26 12:17:25 -0400

    added trigger filter

--- public/js/controllers/triggers-controller.js
+++ public/js/controllers/triggers-controller.js
@@ -1,7 +1,9 @@
 angular.module('iotdash')
-  .controller('triggersController', function($scope, $http) {
+  .controller('triggersController', function($scope, $http, $filter) {
     $scope.newTrigger = {};
     $scope.triggers = [];
+    $scope.filteredTriggers = [];
+    $scope.triggerFilter = "";
 
     //Post a new trigger
     $scope.postNewTrigger = function() {
@@ -24,11 +26,23 @@ angular.module('iotdash')
     $scope.getTriggers = function () {
       $http.get('/triggers/all').success(function (triggers) {
         $scope.triggers = triggers;
+        //Filter through updated set of triggers
+        filterTriggers();
       }).error(function (err) {
         console.log('Failed to get triggers: ' + err);
         $scope.triggers = [];
       });
     };
+
+    //Filter through $scope.triggers and put results into $scope.filteredTriggers
+    function filterTriggers() {
+      $scope.filteredTriggers = $filter('filter')($scope.triggers, {$: $scope.triggerFilter});
+    }
+
+    //Run whenever the search box is typed into
+    $scope.$watch('triggerFilter', function() {
+      filterTriggers();
+    });
 
     //Update trigger list every minute
     setInterval(function () {
