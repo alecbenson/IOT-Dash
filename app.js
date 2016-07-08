bac1e8f0baa88b8e130469e64aba4d1d789ca9ab
alec <abenson@wpi.edu>
2016-07-07 23:22:10 -0400

    more lint

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+'use strict';
+
 var program = require('commander');
 var winston = require('winston');
-var bodyParser = require('body-parser');
 var db = require('./db');
 
 //Used to specify the port that the application runs on
 program
-  .version('0.0.1')
-  .option('-p, --port <n>', 'the port to run the server on', Number, 8080)
-  .parse(process.argv);
+	.version('0.0.1')
+	.option('-p, --port <n>', 'the port to run the server on', Number, 8080)
+	.parse(process.argv);
 
 //Set up express
 var express = require('express');
@@ -22,16 +23,16 @@ app.use(require('./controllers'));
 db.connect();
 
 //Listen on the specified port
-app.listen(program.port, function() {
-  winston.log('info', 'Node process ' + process.pid +
-    ' is listening on port ' + program.port);
+var server = app.listen(program.port, function () {
+	winston.log('info', 'Node process ' + process.pid +
+		' is listening on port ' + program.port);
 });
 
 //Exit nicely on SIGTERM
-process.on('SIGTERM', function() {
-  server.close(function() {
-    winston.log('info', 'bye');
-    db.close();
-    process.exit(0);
-  });
+process.on('SIGTERM', function () {
+	server.close(function () {
+		winston.log('info', 'bye');
+		db.close();
+		process.exit(0);
+	});
 });
--- gulpfile.js
+++ gulpfile.js
@@ -7,47 +7,63 @@ var base = '';
 var dest = 'public/dist/';
 var vendorDest = 'public/dist/vendor/';
 //Filters for file types
-const jsFilter = plugins.filter('**/*.js', {restore: true});
-const lessFilter = plugins.filter('**/*.less', {restore: true});
-const sassFilter = plugins.filter('**/*.scss', {restore: true});
-const cssFilter = plugins.filter('**/*.css', {restore: true});
+const jsFilter = plugins.filter('**/*.js', {
+	restore: true
+});
+const lessFilter = plugins.filter('**/*.less', {
+	restore: true
+});
+const sassFilter = plugins.filter('**/*.scss', {
+	restore: true
+});
+const cssFilter = plugins.filter('**/*.css', {
+	restore: true
+});
 
 const mainPaths = {
-  js: [
-    'public/js/module.js',
-    'public/js/directives/**/*.js',
-    'public/js/controllers/**/*.js'
-  ],
-  css: [
-    'public/css/app.css'
-  ]
+	js: [
+		'public/js/module.js',
+		'public/js/directives/**/*.js',
+		'public/js/controllers/**/*.js'
+	],
+	css: [
+		'public/css/app.css'
+	]
 };
 
 //Run bower
-gulp.task('bower', function() {
-  return plugins.bower();
+gulp.task('bower', function () {
+	return plugins.bower();
 });
 
 //Concatenate and minify iotdash JS
-gulp.task('js', function() {
-	gulp.src(mainPaths.js, {base: base})
+gulp.task('js', function () {
+	gulp.src(mainPaths.js, {
+			base: base
+		})
 		.pipe(plugins.concat('main.js'))
-		.pipe(plugins.uglify({mangle: false}))
+		.pipe(plugins.uglify({
+			mangle: false
+		}))
 		.pipe(gulp.dest(dest + 'js'));
 });
 
 //Concatenate and minify vendor CSS
-gulp.task('css', function() {
-  gulp.src(mainPaths.css, {base: base})
-    .pipe(plugins.concat('app.css'))
-    //.pipe(plugins.uglifycss())
-    .pipe(gulp.dest(dest + 'css'));
+gulp.task('css', function () {
+	gulp.src(mainPaths.css, {
+			base: base
+		})
+		.pipe(plugins.concat('app.css'))
+		//.pipe(plugins.uglifycss())
+		.pipe(gulp.dest(dest + 'css'));
 });
 
 //Concatenate and minify vendor JS
-gulp.task('vendorJs', function() {
-	gulp.src('./bower.json', {base: base})
-    .pipe(plugins.mainBowerFiles())
+gulp.task('vendorJs', function () {
+	gulp.src('./bower.json', {
+			base: base
+		})
+		.pipe(plugins.mainBowerFiles())
 		.pipe(jsFilter)
 		.pipe(plugins.concat('vendor.js'))
 		.pipe(plugins.uglify())
@@ -55,44 +71,52 @@ gulp.task('vendorJs', function() {
 });
 
 //Concatenate and minify vendor CSS
-gulp.task('vendorCss', function() {
-  var lessStream = gulp.src('./bower.json', {base: base})
-    .pipe(plugins.mainBowerFiles())
+gulp.task('vendorCss', function () {
+	var lessStream = gulp.src('./bower.json', {
+			base: base
+		})
+		.pipe(plugins.mainBowerFiles())
 		.pipe(lessFilter)
-    .pipe(plugins.less())
-    .pipe(plugins.concat('less-files.less'));
+		.pipe(plugins.less())
+		.pipe(plugins.concat('less-files.less'));
 
-  var scssStream = gulp.src('./bower.json', {base: base})
-    .pipe(plugins.mainBowerFiles())
+	var scssStream = gulp.src('./bower.json', {
+			base: base
+		})
+		.pipe(plugins.mainBowerFiles())
 		.pipe(sassFilter)
-    .pipe(plugins.sass())
-    .pipe(plugins.concat('scss-files.less'));
+		.pipe(plugins.sass())
+		.pipe(plugins.concat('scss-files.less'));
 
-  var cssStream = gulp.src('./bower.json', {base: base})
-    .pipe(plugins.mainBowerFiles())
+	var cssStream = gulp.src('./bower.json', {
+			base: base
+		})
+		.pipe(plugins.mainBowerFiles())
 		.pipe(cssFilter)
-    .pipe(plugins.concat('css-files.css'));
+		.pipe(plugins.concat('css-files.css'));
 
-  var mergedStream = merge(lessStream, scssStream, cssStream)
-    .pipe(plugins.concat('style.css'))
-    .pipe(plugins.uglifycss())
-    .pipe(gulp.dest(vendorDest + 'css'));
+	var mergedStream = merge(lessStream, scssStream, cssStream)
+		.pipe(plugins.concat('style.css'))
+		.pipe(plugins.uglifycss())
+		.pipe(gulp.dest(vendorDest + 'css'));
 
-  return mergedStream;
+	return mergedStream;
 });
 
 gulp.task('vendorFonts', function () {
-  return gulp.src('./bower_components/**/*.{eot,svg,ttf,woff,woff2}', {base: base})
-    .pipe(plugins.flatten())
-    .pipe(gulp.dest(vendorDest + 'fonts'));
+	return gulp.src('./bower_components/**/*.{eot,svg,ttf,woff,woff2}', {
+			base: base
+		})
+		.pipe(plugins.flatten())
+		.pipe(gulp.dest(vendorDest + 'fonts'));
 });
 
 //Lint everything using elsint
-gulp.task('lint', function() {
-  return gulp.src(['*/**.js','!node_modules/**', '!bower_components/**'])
-    .pipe(plugins.eslint())
-    .pipe(plugins.eslint.format())
-    .pipe(plugins.eslint.failAfterError());
+gulp.task('lint', function () {
+	return gulp.src(['db.js', 'app.js', '*/**.js', '!node_modules/**', '!bower_components/**'])
+		.pipe(plugins.eslint())
+		.pipe(plugins.eslint.format())
+		.pipe(plugins.eslint.failAfterError());
 });
 
 gulp.task('default', ['bower', 'lint', 'js', 'css', 'vendorFonts', 'vendorJs', 'vendorCss']);
