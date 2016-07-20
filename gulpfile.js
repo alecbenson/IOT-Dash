4661725066f3f4e4eabaa8763e767d25c429b177
alec <abenson@wpi.edu>
2016-07-19 23:37:42 -0400

    Switched to angular material package

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,21 @@
 'use strict';
 
 var gulp = require('gulp');
-// Include plugins
-var plugins = require("gulp-load-plugins")();
 var merge = require('merge-stream');
+var uglify = require('gulp-uglify');
+var uglifycss = require('gulp-uglifycss');
+var bower = require('gulp-bower');
+var concat = require('gulp-concat');
+var mainBowerFiles = require('main-bower-files');
+var sass = require('gulp-sass');
+var less = require('gulp-less');
+var flatten = require('gulp-flatten');
+var eslint = require('gulp-eslint');
+
 //Output destination
 var base = '';
 var dest = 'public/dist/';
 var vendorDest = 'public/dist/vendor/';
-//Filters for file types
-const jsFilter = plugins.filter('**/*.js', {
-	restore: true
-});
-const lessFilter = plugins.filter('**/*.less', {
-	restore: true
-});
-const sassFilter = plugins.filter('**/*.scss', {
-	restore: true
-});
-const cssFilter = plugins.filter('**/*.css', {
-	restore: true
-});
 
 const mainPaths = {
 	js: [
@@ -35,16 +30,14 @@ const mainPaths = {
 
 //Run bower
 gulp.task('bower', function () {
-	return plugins.bower();
+	return bower();
 });
 
 //Concatenate and minify iotdash JS
 gulp.task('js', function () {
-	gulp.src(mainPaths.js, {
-			base: base
-		})
-		.pipe(plugins.concat('main.js'))
-		.pipe(plugins.uglify({
+	gulp.src(mainPaths.js)
+		.pipe(concat('main.js'))
+		.pipe(uglify({
 			mangle: false
 		}))
 		.pipe(gulp.dest(dest + 'js'));
@@ -55,51 +48,35 @@ gulp.task('css', function () {
 	gulp.src(mainPaths.css, {
 			base: base
 		})
-		.pipe(plugins.concat('app.css'))
-		.pipe(plugins.uglifycss())
+		.pipe(concat('app.css'))
+		.pipe(uglifycss())
 		.pipe(gulp.dest(dest + 'css'));
 });
 
 //Concatenate and minify vendor JS
 gulp.task('vendorJs', function () {
-	gulp.src('./bower.json', {
-			base: base
-		})
-		.pipe(plugins.mainBowerFiles())
-		.pipe(jsFilter)
-		.pipe(plugins.concat('vendor.js'))
-		.pipe(plugins.uglify())
+	gulp.src(mainBowerFiles('**/*.js'))
+		.pipe(concat('vendor.js'))
+		.pipe(uglify())
 		.pipe(gulp.dest(vendorDest + 'js'));
 });
 
 //Concatenate and minify vendor CSS
 gulp.task('vendorCss', function () {
-	var lessStream = gulp.src('./bower.json', {
-			base: base
-		})
-		.pipe(plugins.mainBowerFiles())
-		.pipe(lessFilter)
-		.pipe(plugins.less())
-		.pipe(plugins.concat('less-files.less'));
+	var lessStream = gulp.src(mainBowerFiles('**/*.less'))
+		.pipe(less())
+		.pipe(concat('less-files.less'));
 
-	var scssStream = gulp.src('./bower.json', {
-			base: base
-		})
-		.pipe(plugins.mainBowerFiles())
-		.pipe(sassFilter)
-		.pipe(plugins.sass())
-		.pipe(plugins.concat('scss-files.less'));
+	var scssStream = gulp.src(mainBowerFiles('**/*.scss'))
+		.pipe(sass())
+		.pipe(concat('scss-files.scss'));
 
-	var cssStream = gulp.src('./bower.json', {
-			base: base
-		})
-		.pipe(plugins.mainBowerFiles())
-		.pipe(cssFilter)
-		.pipe(plugins.concat('css-files.css'));
+	var cssStream = gulp.src(mainBowerFiles('**/*.css'))
+		.pipe(concat('css-files.css'));
 
-	var mergedStream = merge(lessStream, scssStream, cssStream)
-		.pipe(plugins.concat('style.css'))
-		.pipe(plugins.uglifycss())
+	var mergedStream = merge(scssStream, lessStream, cssStream)
+		.pipe(concat('style.css'))
+		.pipe(uglifycss())
 		.pipe(gulp.dest(vendorDest + 'css'));
 
 	return mergedStream;
@@ -109,16 +86,16 @@ gulp.task('vendorFonts', function () {
 	return gulp.src('./bower_components/**/*.{eot,svg,ttf,woff,woff2}', {
 			base: base
 		})
-		.pipe(plugins.flatten())
+		.pipe(flatten())
 		.pipe(gulp.dest(vendorDest + 'fonts'));
 });
 
 //Lint everything using elsint
 gulp.task('lint', function () {
 	return gulp.src(['**/**.js', '!gulpfile.js', '!public/dist/**', '!node_modules/**', '!bower_components/**'])
-		.pipe(plugins.eslint())
-		.pipe(plugins.eslint.format())
-		.pipe(plugins.eslint.failAfterError());
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
 });
 
 gulp.task('default', ['bower', 'lint', 'js', 'css', 'vendorFonts', 'vendorJs', 'vendorCss']);
--- public/js/module.js
+++ public/js/module.js
@@ -1,6 +1,3 @@
 'use strict';
 
-angular.module('iotdash', []);
-$(document).ready(function () {
-	$.material.init();
-});
+angular.module('iotdash', ['angularBootstrapMaterial']);
